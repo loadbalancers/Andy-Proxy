22973f06bfae8c2a44a425f32166747a583547a6
Andy Siamas <andysiamas@Andys-MacBook-Air.local>
2018-10-11 19:27:04 -0700

    Proxy successfully connects all servers

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require('newrelic');
 const express = require ('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const request = require('request');
 var compression = require('compression');
 
 var server = express();
@@ -12,23 +14,27 @@ server.use(express.static(path.join(__dirname, './'), { maxAge: '30 days' }));
 server.use(cors());
 
 // Albums & Player
-server.get('/artists/albums/:artistID', (req, res) => {
-  res.redirect('http://52.15.129.193' + req.url);
+server.get('/artists/:artistId/albums', (req, res) => {
+  req.pipe(request('http://13.57.38.215:3000' + req.url)).pipe(res);
 });
 
 // Related Artists
-server.get('/artist/:id/relatedArtists', (req, res) => {
-  res.redirect('http://18.206.245.56' + req.url);
+server.get('*/related-artists', (req, res) => {
+  res.redirect(`http://34.239.128.16:3005/ra-module/${Math.floor(Math.random()*1000)}/related-artists`);
 });
 
 // Popular Songs
 server.get('/artist/:id', (req, res) => {
-  res.redirect('http://18.224.17.253' + req.url);
+  res.redirect('http://13.56.254.143' + req.url);
 });
 
 // Header
 server.get('/artists/:artistID', (req, res) => {
-   res.redirect('http://35.172.133.115' + req.url);
+   res.redirect('http://54.193.126.17:3004' + req.url);
+});
+
+server.get('/locations', (req, res) => {
+  res.redirect('http://54.193.126.17:3004' + req.url);
 });
 
 server.listen(3000, console.log('Listening on:', 3000));
